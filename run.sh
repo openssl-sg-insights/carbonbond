d3af56f767889b0814f6d8bdb4d0e68b3b0d7ea1
fro11o <tcps195283@gmail.com>
2022-06-11 22:23:11 +0800

    將 frontend 直接搬到 packages/desktop 中

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ sudo service postgresql start
 cd api-service
 tmux new-session -s $NAME -d "env RUST_LOG=debug cargo run; bash"
 cd ../frontend
-tmux split-window -h "yarn dev; bash"
-tmux split-window -v "yarn check-ts --watch; bash"
+tmux split-window -h "yarn workspace @frontend/desktop dev; bash"
+tmux split-window -v "yarn workspace @frontend/desktop check-ts --watch; bash"
 
 tmux -2 attach-session -d
